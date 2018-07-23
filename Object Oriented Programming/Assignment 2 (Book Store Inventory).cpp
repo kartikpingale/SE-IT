dd13740971b33959e0adbf8401b524691fca226d
Kartik Pingale <kartikpgr8@gmail.com>
2018-07-23 22:55:38 +0530

    Update Assignment 2 (Book Store Inventory).cpp

--- Object Oriented Programming/Assignment 2 (Book Store Inventory).cpp
+++ Object Oriented Programming/Assignment 2 (Book Store Inventory).cpp
@@ -1,1 +1,18 @@
+/*
+A Book shop maintains the inventory of books that are being sold at the shop. The list includes details such as title, author, publisher, price and available stock.
+Write a program in C++ which will have a class called books with suitable member functions for
+i. Add
+ii. Update
+iii. Search a book
+iv. Purchase a book (update the stock and display the total cost)
+v. Record number of successful/unsuccessful transactions (use static data
+members to keep count of transactions)
+Use new operator in constructors to allocate memory space required.
+*/
 
+#include<iostream>
+
+int main()
+{
+    return 0;
+}
