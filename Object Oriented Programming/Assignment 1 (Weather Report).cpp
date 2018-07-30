afba25a5de28cbc8916f88d5cba52cd1a91e6f42
Kartik Pingale <kartikpgr8@gmail.com>
2018-07-30 21:25:39 +0530

    Update Assignment 1 (Weather Report).cpp

--- Object Oriented Programming/Assignment 1 (Weather Report).cpp
+++ Object Oriented Programming/Assignment 1 (Weather Report).cpp
@@ -19,89 +19,172 @@ class weatherReport
 		avg_amountRain=0.0;
 		avg_amountSnow=0.0;
 	}
-	void getData(weatherReport day[50], int n)
+	void getData(weatherReport day[][32], int month)
 	{
-        int i;
-        for(i=0; i<n; i++)
+        int i, maxDays, date=1;
+        char repeat='y';
+        maxDays=getDays(month);
+        
+        while(repeat=='y' || repeat=='Y')
         {
-            cout << "\nEnter high temperature: ";
-            cin >> day[i].highTemp;
-            cout << "Enter low temperature: ";
-			cin >> day[i].lowTemp;
-			cout << "Enter amount of rain: ";
-			cin >> day[i].amountRain;
-			cout << "Enter amount of snow: ";
-			cin >> day[i].amountSnow;
+        	do
+			{
+				if(date<1 || date>maxDays)
+				{
+					cout << "Invalid selection! Enter a date between 1-"<< maxDays <<"!\n";
+				}
+				cout << "\nEnter date: ";
+				cin >> date;
+				if(date>=1 && date<=maxDays)
+				{
+					cout << "\nEnter high temperature: ";
+            		cin >> day[month][date].highTemp;
+            		cout << "Enter low temperature: ";
+					cin >> day[month][date].lowTemp;
+					cout << "Enter amount of rain: ";
+					cin >> day[month][date].amountRain;
+					cout << "Enter amount of snow: ";
+					cin >> day[month][date].amountSnow;
+					cout << "\nDo you want to enter data for another day? (y/n)";
+					cin >> repeat;
+				}
+			}
+            while(date<1 || date>maxDays);
         }
 	}
-	void modifyData(weatherReport day[50], int n)
-	{
-        int dayNumber, userDay;
-		cout << "\nEnter day for which you want to modify the data: ";
-		cin >> userDay;
-        if(userDay<=n)
+    void putData(weatherReport day[][32], int month)
+    {
+        int maxDays, date;
+        maxDays=getDays(month);
+        cout << "\nEnter date: ";
+        cin >> date;
+        if(date>=1 && date<=maxDays)
         {
-            dayNumber=(userDay-1);
-            cout << "Enter high temperature: ";
-            cin >> day[dayNumber].highTemp;
-            cout << "Enter low temperature: ";
-		    cin >> day[dayNumber].lowTemp;
-		    cout << "Enter amount of rain: ";
-		    cin >> day[dayNumber].amountRain;
-		    cout << "Enter amount of snow: ";
-		    cin >> day[dayNumber].amountSnow;
+            cout << "\nHigh temperature: " << day[month][date].highTemp << endl;
+            cout << "Low temperature: " << day[month][date].lowTemp << endl;
+			cout << "Amount of rain: " << day[month][date].amountRain << endl;
+			cout << "Amount of snow: " << day[month][date].amountSnow << endl;
         }
         else
         {
-            cout << "Enter correct day to modify data!";
+            cout << "Invalid selection! Enter a date between 1-"<< maxDays <<"!\n";
         }
-	}
-	void putData(weatherReport day[50], int n)
+    }
+    void modifyData(weatherReport day[][32], int month)
 	{
-        int i;
-        for(i=0; i<n; i++)
+        int i, maxDays, date=1;
+        char repeat='y';
+        maxDays=getDays(month);
+        
+        while(repeat=='y' || repeat=='Y')
         {
-            cout << "\nDay " << (i+1) << ":\n";
-            cout << "High temperature is " << day[i].highTemp << " C" << endl;
-		    cout << "Low temperature is " << day[i].lowTemp << " C" << endl;
-		    cout << "Amount of rain is " << day[i].amountRain << " mm" << endl;
-		    cout << "Amount of snow is " << day[i].amountSnow << " mm" << endl;   
+        	do
+			{
+				if(date<1 || date>maxDays)
+				{
+					cout << "Invalid selection! Enter a date between 1-"<< maxDays <<"!\n";
+				}
+				cout << "\nEnter date to modify data: ";
+				cin >> date;
+				if(date>=1 && date<=maxDays)
+				{
+					cout << "\nEnter high temperature: ";
+            		cin >> day[month][date].highTemp;
+            		cout << "Enter low temperature: ";
+					cin >> day[month][date].lowTemp;
+					cout << "Enter amount of rain: ";
+					cin >> day[month][date].amountRain;
+					cout << "Enter amount of snow: ";
+					cin >> day[month][date].amountSnow;
+					cout << "\nDo you want to modify data for another day? (y/n)";
+					cin >> repeat;
+				}
+			}
+            while(date<1 || date>maxDays);
         }
+        cout << "Data has been modified!"
 	}
-	void generateReport(weatherReport day[50], int n)
-	{
-        int i;
+    void generateReport(weatherReport day[][32], int month)
+    {
+        int i, maxDays, days=0;
+        maxDays=getDays(month);
         float sumHigh=0, sumLow=0, sumRain=0, sumSnow=0;
-        for(i=0; i<n; i++)
+        for(i=1; i<=maxDays; i++)
         {
-            sumHigh+=day[i].highTemp;
-            sumLow+=day[i].lowTemp;
-            sumRain+=day[i].amountRain;
-            sumSnow+=day[i].amountSnow;
+            sumHigh+=day[month][i].highTemp;
+            sumLow+=day[month][i].lowTemp;
+            sumRain+=day[month][i].amountRain;
+            sumSnow+=day[month][i].amountSnow;
+            if(day[month][i].highTemp!=0 && day[month][i].lowTemp!=0 && day[month][i].amountRain!=0 && day[month][i].amountSnow!=0)
+            {
+                days++;
+            }
         }
-        avg_highTemp=(sumHigh/n);
-        avg_lowTemp=(sumLow/n);
-        avg_amountRain=(sumRain/n);
-        avg_amountSnow=(sumSnow/n);
+        avg_highTemp=(sumHigh/days);
+        avg_lowTemp=(sumLow/days);
+        avg_amountRain=(sumRain/days);
+        avg_amountSnow=(sumSnow/days);
 		cout << "\nDay\tHigh Temperature\tLow Temperature\t\tAmount of rain\t\tAmount of snow\n";
-		for(i=0; i<n; i++)
+		for(i=1; i<=maxDays; i++)
         {
-            cout << (i+1) << "\t\t" << day[i].highTemp << "\t\t\t" << day[i].lowTemp << "\t\t\t" << day[i].amountRain << "\t\t\t" << day[i].amountSnow << endl;
+            cout << (i) << "\t\t" << day[month][i].highTemp << "\t\t\t" << day[month][i].lowTemp << "\t\t\t" << day[month][i].amountRain << "\t\t\t" << day[month][i].amountSnow << endl;
         }
         cout << "\nAverage high temperature: " << avg_highTemp;
         cout << "\nAverage low temperature: " << avg_lowTemp;
         cout << "\nAverage amount of rain: " << avg_amountRain;
         cout << "\nAverage amount of snow: " << avg_amountSnow << endl;
-	}
+    }
+    int getDays(int month)
+    {
+        int maxDays;
+        switch(month)
+        {
+        	case 1:
+        	case 3:
+        	case 5:
+        	case 7:
+        	case 8:
+        	case 10:
+        	case 12:
+        	{
+        		maxDays=31;
+        		break;
+        	}
+        	case 4:
+        	case 6:
+        	case 9:
+        	case 11:
+        	{
+        		maxDays=30;
+        		break;
+        	}
+        	case 2:
+        	{
+        		maxDays=29;
+        		break;
+        	}
+        }
+        return maxDays;
+    }
 };
 
 int main()
 {
-	weatherReport today, day[50];
-	int choice, countDays;
+	weatherReport today, day[13][32];
+	int choice, month=1;
 	char repeat='y';
-    cout << "Enter no. of days: ";
-    cin >> countDays;
+	
+	do
+	{
+		if(month<1 || month>12)
+		{
+			cout << "Invalid selection! Enter a month between 1-12!\n";
+		}
+		cout << "Enter month (1-12): ";
+		cin >> month;
+	}
+	while(month<1 || month>12);
+	
 	while(repeat=='y' || repeat=='Y')
 	{
 		cout << "\n1. Enter data\n2. Display data\n3. Modify data\n4. Generate report\nSelect the operation: ";
@@ -110,27 +193,27 @@ int main()
 		{
 			case 1:
 				{
-					today.getData(day, countDays);
+					today.getData(day, month);
 					break;
 				}
 			case 2:
 				{
-					today.putData(day, countDays);
+					today.putData(day, month);
 					break;
 				}
 			case 3:
 				{
-					today.modifyData(day, countDays);
+					today.modifyData(day, month);
 					break;
 				}
 			case 4:
 				{
-					today.generateReport(day, countDays);
+					today.generateReport(day, month);
 					break;
 				}
 			default:
 				{
-					cout << "Invalid selection! Select a number between 1-3";
+					cout << "Invalid selection! Select a number between 1-4";
 					break;
 				}
 		}
