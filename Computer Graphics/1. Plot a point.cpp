3f757ffa5631c5b74112baa5ab2ae3f835a3cdc7
Kartik Pingale <kartikpgr8@gmail.com>
2019-04-13 13:23:32 +0530

    Create 1. Plot a point.cpp

---
+++ Computer Graphics/1. Plot a point.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <GL/glut.h>
+
+#define w 640
+#define h 480
+
+using namespace std;
+
+void myInit()
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glColor3f(0.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glLineWidth(2.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+}
+
+void myDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_POINTS);
+			glVertex2i(100,100);
+		glEnd();
+	glFlush();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(w, h);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Display Point");
+	glutDisplayFunc(myDisplay);
+	myInit();
+	glutMainLoop();
+	return 0;
+}
