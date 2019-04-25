bf3c62e056260fc18a70c393cd8693cf84050d42
Kartik Pingale <kartikpgr8@gmail.com>
2019-04-25 08:06:44 +0530

    Update Assignment 8.asm

--- Processor Interfacing/Assignment 8.asm
+++ Processor Interfacing/Assignment 8.asm
@@ -1,1 +1,28 @@
+; PROBLEM STATEMENT
+; Write 8051 ALP to multiply 16 bit number by 8 bit number and store the result in internal memory location.
+;
+; AUTHOR - Kartik Pingale
 
+mov R0,#40h
+mov R1,#50h
+
+mov A,@R0
+mov B,@R1
+
+mul AB
+
+mov R7,A
+mov R5,B
+
+inc R0
+
+mov A,@R0
+mov B,@R1
+
+mul AB
+
+add A,R5
+mov A,B
+addc A,00h
+
+lcall 0055
